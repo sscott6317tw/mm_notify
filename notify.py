d7c9d4cca5ee6f8c528303b890a8734b9470c7a5
ralflinS <ralflin@knowhow.idv.tw>
2024-05-23 12:06:23 +0800

    update

--- notify.py
+++ notify.py
@@ -1,7 +1,9 @@
 from asyncio import IncompleteReadError
 from asyncio import run as async_run
-from datetime import datetime
-from random import choice
+from time import mktime, strptime, time
+import os
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
 
 from aiohttp import ClientSession, request
 from aiowebsocket.converses import AioWebSocket
@@ -18,8 +20,8 @@ class MMBot:
         self.config.read("config\config.ini")
         self.server_url = "pmo-mattermost.qyrc452.com"
         self.eng_name = self.config['tg_info']['eng_name']
-        self.username = self.config['mm_info']['username']
-        self.password = self.config['mm_info']['password']
+        self.username = self.config['mm_info'][f'{user}_username']
+        self.password = self.config['mm_info'][f'{user}_password']
         self.token = None
         self.headers = None
         self.team_name = team_name
@@ -35,7 +37,7 @@ class MMBot:
     def get_new_key_user(self):
         self.config = configparser.ConfigParser() 
         self.config.read("config\config.ini")
-        key_user = self.config['mm_info']['keypoint_user'].split(",")
+        key_user = self.config['mm_info'][f'{user}_keypoint_user'].split(",")
         return key_user
 
     async def login_get_token(self, bot=False):
@@ -97,11 +99,41 @@ class MMBot:
                                         f"注意注意!!{r['data']['sender_name']}在群組{r['data']['channel_display_name']}標記全部人\n訊息內容：{loads(r['data']['post'])['message']}"
                                     )
                                     telegram_post_msg(self.eng_name, msg)
-                        elif r.get("event") and r.get("event") == 'status_change' and r['data']['status'] =="away": #防止離線方式
-                            return
+                        elif r.get("event") and r.get("event") == 'status_change' : #防止離線方式
+                            await self.send_private_message("test")
+                            msg = (
+                                "你離線啦!!!!!!!幫你傳個訊息給別人 Hee Ha~~~~~~~"
+                            )
+                            telegram_post_msg(self.eng_name, msg)       
                         logger.info(dumps(r, option=OPT_INDENT_2).decode("utf-8-sig"))
+                    check_endtime(end_time)                 
             except IncompleteReadError as e:
                 print(e, "IncompleteReadError")
+
+    async def send_private_message(self, message):
+        mm_bot = MMBot("qa-ornd", "api")
+        mm_bot.token = await mm_bot.login_get_token()
+        url = f"https://{self.server_url}/api/v4"
+        headers = self.headers = {"Authorization": f"Bearer {mm_bot.token}"}
+        #Kevin err4uojxsj8mmysd1yt1mjhcpr
+        #Joey pzzxp9nni3nctrkdmm131me41h
+        send_msg = {
+            "ralf": "pzzxp9nni3nctrkdmm131me41h", #後面是要送的人
+            "joey": "err4uojxsj8mmysd1yt1mjhcpr"
+            }
+        async with ClientSession(headers=headers) as session:
+            content = {"channel_id": send_msg[user], "message": message}
+            async with session.post(f"{url}/posts", json=content) as resp:
+                msg_id = await resp.json()
+        return message
+
+def check_endtime(time_string):
+    if time_string != "0":
+        time_stamp = int(mktime(strptime(time_string, "%Y-%m-%d %H:%M")))  # 轉成時間戳
+        now = int(time())
+        if now >= time_stamp:
+            logger.success("預計結束時間到期")
+            os._exit(0)
 
 def telegram_post_msg(user_name, msg):
     send_message_to_user(user_name, msg)
@@ -109,7 +141,19 @@ def telegram_post_msg(user_name, msg):
 def linebot_post_msg(user_name, msg):
     send_message_to_channel(user_name, msg)
 
+def parse_args():
+    parser = ArgumentParser(prog="notify.py")
+    parser.add_argument("-u", "--user", type=str)
+    return parser.parse_args()
+
 if __name__ == "__main__":
+    args = parse_args()
+    if args.user:
+        user = args.user
+    else:
+        user = "ralf"
+    new_time = datetime.now() + timedelta(minutes=5)
+    end_time = new_time.strftime("%Y-%m-%d %H:%M")
     method = MMBot("qa-ornd", method="websocket")
     while True:
         async_run(method.connect_to_mattermost())
